7246d2d9ad7cf1df74d60d54b9fbe12f16e6a2bf
Nozomi Hosone <otofuto@gmail.com>
2021-11-21 20:19:56 +0900

    3

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/joho/godotenv v1.4.0
 	github.com/lib/pq v1.10.4
+	golang.org/x/net v0.0.0-20211118161319-6a13c67c3ce4 // indirect
 )
--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"html/template"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/joho/godotenv"
 	"github.com/otofuto/yotuba-teiru/pkg/database"
+	"golang.org/x/net/html"
 )
 
 var port string
@@ -28,6 +33,16 @@ type Comment struct {
 	ReplyTo  int `json:"replyto"`
 }
 
+type Ogp struct {
+	Image       string `json:"image"`
+	ImageToOgp  string `json:"image_to_ogp"`
+	Url         string `json:"url"`
+	Title       string `json:"title"`
+	SiteName    string `json:"site_name"`
+	Description string `json:"description"`
+	Valid       bool   `json:"valid"`
+}
+
 func main() {
 	_ = godotenv.Load()
 	port = os.Getenv("PORT")
@@ -38,6 +53,8 @@ func main() {
 	http.Handle("/st/", http.StripPrefix("/st/", http.FileServer(http.Dir("./static"))))
 	http.HandleFunc("/", IndexHandle)
 	http.HandleFunc("/favicon.ico", FaviconHandle)
+	http.HandleFunc("/ogpimg/", OgpImgHandle)
+	http.HandleFunc("/ogp/", OgpHandle)
 
 	log.Println("Listening on port: " + port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
@@ -89,6 +106,112 @@ func FaviconHandle(w http.ResponseWriter, r *http.Request) {
 	}
 	defer file.Close()
 	io.Copy(w, file)
+}
+
+func OgpImgHandle(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", r.Header.Get("Content-Type"))
+
+	if r.Method == http.MethodGet {
+		url := r.FormValue("url")
+		if url == "" {
+			http.Error(w, "Parameter 'url' does not allow empty.", 400)
+		} else {
+			res, err := http.Get(url)
+			if err != nil {
+				log.Println(err)
+				http.Error(w, "Access denied.", 500)
+				return
+			}
+			defer res.Body.Close()
+			io.Copy(w, res.Body)
+		}
+	} else {
+		http.Error(w, "Supported method is only 'GET'.", http.StatusMethodNotAllowed)
+	}
+}
+
+func OgpHandle(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method == http.MethodGet {
+		url := r.FormValue("url")
+		if url == "" {
+			http.Error(w, "Parameter 'url' does not allow empty.", 400)
+		} else {
+			ogp := GetOgp(url)
+			bytes, _ := json.Marshal(ogp)
+			fmt.Fprintf(w, string(bytes))
+		}
+	} else {
+		http.Error(w, "Supported method is only 'GET'.", http.StatusMethodNotAllowed)
+	}
+}
+
+func GetOgp(url string) Ogp {
+	ret := Ogp{
+		Valid: false,
+	}
+	if strings.TrimSpace(url) == "" {
+		return ret
+	}
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Println("main.go GetOgp(url string)")
+		log.Println(err)
+		return ret
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	node, err := html.Parse(strings.NewReader(bytes.NewBuffer(body).String()))
+	meta := findNodesByTagName(node, "meta")
+	for _, m := range meta {
+		if prop := getAttribute(m, "property"); strings.HasPrefix(prop, "og:") {
+			if prop == "og:image" {
+				ret.Image = getAttribute(m, "content")
+				ret.ImageToOgp = getAttribute(m, "content")
+				if strings.HasPrefix(ret.Image, "http://") {
+					ret.Image = "/ogpimg?url=" + ret.Image
+					ret.ImageToOgp = "https://yotuba-teiru.herokuapp.com/ogpimg?url=" + ret.ImageToOgp
+				}
+			} else if prop == "og:url" {
+				ret.Url = getAttribute(m, "content")
+			} else if prop == "og:title" {
+				ret.Title = getAttribute(m, "content")
+			} else if prop == "og:site_name" {
+				ret.SiteName = getAttribute(m, "content")
+			} else if prop == "og:description" {
+				ret.Description = getAttribute(m, "content")
+			}
+		}
+	}
+	if ret.Image != "" || ret.Url != "" || ret.Title != "" || ret.SiteName != "" || ret.Description != "" {
+		ret.Valid = true
+	}
+
+	return ret
+}
+
+func findNodesByTagName(parent *html.Node, tagname string) []*html.Node {
+	nodes := make([]*html.Node, 0)
+	for child := parent.FirstChild; child != nil; child = child.NextSibling {
+		if child.Data == tagname {
+			nodes = append(nodes, child)
+		}
+		ns := findNodesByTagName(child, tagname)
+		for _, n := range ns {
+			nodes = append(nodes, n)
+		}
+	}
+	return nodes
+}
+
+func getAttribute(node *html.Node, attrname string) string {
+	for _, attr := range node.Attr {
+		if attr.Key == attrname {
+			return attr.Val
+		}
+	}
+	return ""
 }
 
 //GETでは使えない
